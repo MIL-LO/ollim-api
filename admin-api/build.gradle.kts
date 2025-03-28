33df569d9e1a9c19967bcfbfb41853ca88bb7146
악덕 <rudejrqkr97@gmail.com>
2025-03-28 17:26:13 +0900

    #1 feat(admin-api): admin-api 모듈에 Swagger, 헬스 체크 API 추가
    - application.properties 파일을 application.yml 파일로 변경
    - private 프로파일 활성화 설정 추가
    - Swagger 설정 추가하여 API 문서화 지원
    - 헬스 체크 API 추가하여 서버 상태 확인 가능
    - Swagger 라이브러리 추가

--- admin-api/build.gradle.kts
+++ admin-api/build.gradle.kts
@@ -9,7 +9,11 @@ dependencies {
 	implementation(project(":core"))
 	implementation(project(":infrastructure"))
 
+    // spring-web
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
 	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
