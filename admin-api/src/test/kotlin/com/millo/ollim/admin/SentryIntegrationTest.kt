83904cb3a729ccd12a6ad6241cdc87f36b8531f9
악덕 <rudejrqkr97@gmail.com>
2025-03-30 16:28:10 +0900

    #3 feat(admin-api): Sentry 모니터링 도구 추가
    - 'build.gradle.kts' 파일에 Sentry 관련 의존성 추가
    - 'application.yml' 파일에 Sentry 설정 추가
    - 'SentryIntegrationTest.kt' 파일 추가하여 Sentry 예외 전송 테스트 진행

---
+++ admin-api/src/test/kotlin/com/millo/ollim/admin/SentryIntegrationTest.kt
@@ -0,0 +1,18 @@
+package com.millo.ollim.admin
+
+import io.sentry.Sentry
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SentryIntegrationTest {
+
+    @Test
+    fun `Sentry에 예외 전송 테스트`() {
+        try {
+            throw Exception("Sentry 테스트 예외입니다.")
+        } catch (e: Exception) {
+            Sentry.captureException(e)
+        }
+    }
+}
