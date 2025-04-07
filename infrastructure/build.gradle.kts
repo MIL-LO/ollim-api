2d0ef6aa446f667af0766f902446dff51974df1d
악덕 <rudejrqkr97@gmail.com>
2025-04-07 15:52:13 +0900

    #5 chore(infrastructure): InfrastructureApplication.kt 파일 삭제 및 build.gradle.kts 수정
    - InfrastructureApplication.kt 파일 삭제로 인한 코드 정리
    - build.gradle.kts 파일에서 JPA, Redis, Mongo, PostgreSQL 관련 라이브러리 활성화를 위한 수정 작업 진행

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -9,10 +9,17 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-//	runtimeOnly("org.postgresql:postgresql")
+    // JPA
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // Mongo
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    // PostgreSQL
+	runtimeOnly("org.postgresql:postgresql")
 
 	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
