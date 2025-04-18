8d5fba1623508820e37649fee567c050f2aff09e
악덕 <rudejrqkr97@gmail.com>
2025-04-18 23:28:42 +0900

    #12 feat(app-api): 데이터베이스 설정 추가
    - application.yml 파일에 PostgreSQL, JPA, Redis 설정 추가
    - AppApplication.kt에 JPA repository, Entity 스캔 설정 추가

--- app-api/src/main/kotlin/com/millo/ollim/app/AppApplication.kt
+++ app-api/src/main/kotlin/com/millo/ollim/app/AppApplication.kt
@@ -1,9 +1,13 @@
 package com.millo.ollim.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["com.millo.ollim.core.domain"])
+@EntityScan(basePackages = ["com.millo.ollim.core.domain"])
 class AppApplication
 
 fun main(args: Array<String>) {
