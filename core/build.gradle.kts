0472b1579d3d9023b998601bfa1c49183de492ff
악덕 <rudejrqkr97@gmail.com>
2025-04-18 23:17:23 +0900

    #12 feat(core): build.gradle.kts에 필요한 의존성 추가
    - Spring Data JPA, Spring Data REST, PostgreSQL, Kotlin 리플렉션, Kotlin 전용 Jackson 직렬화, Jasypt, 테스트 관련 의존성을 추가함

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,16 +5,25 @@ plugins {
 }
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    // Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    // Kotlin
+    // Spring Data REST (HAL 자동 노출 및 Repository 어노테이션 사용 시 필요)
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+
+    // PostgreSQL
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Kotlin 리플렉션
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Kotlin 전용 Jackson 직렬화
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Jasypt
-    implementation ("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
 
+    // 테스트
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
