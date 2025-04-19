21dc2c3836af17ce9aa06478783e578fbc6b170e
악덕 <rudejrqkr97@gmail.com>
2025-04-19 16:14:41 +0900

    #12 feat(app-api): Redis 설정 및 컨트롤러 추가
    - application.yml 파일에서 Redis 설정 변경
    - RedisController.kt 파일 추가하여 Redis 관련 기능 구현
    - AppApplication.kt 파일에서 ComponentScan 추가하여 컴포넌트 스캔 범위 확장

--- app-api/src/main/kotlin/com/millo/ollim/app/AppApplication.kt
+++ app-api/src/main/kotlin/com/millo/ollim/app/AppApplication.kt
@@ -3,11 +3,13 @@ package com.millo.ollim.app
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = ["com.millo.ollim.core.domain"])
 @EntityScan(basePackages = ["com.millo.ollim.core.domain"])
+@ComponentScan(basePackages = ["com.millo.ollim"])
 class AppApplication
 
 fun main(args: Array<String>) {
