62087619ea9832872ff9adcb9aad63636c44f124
악덕 <rudejrqkr97@gmail.com>
2025-04-21 21:06:05 +0900

    #18 refactor: BaseTimeEntity 클래스 패키지 이동 및 주석 추가
    - BaseTimeEntity 클래스의 패키지를 'com.millo.ollim.core.common.entity'에서 'com.millo.ollim.common.domain'으로 이동
    - BaseTimeEntity 클래스에 대한 설명 주석 추가
    - 'createdAt', 'updatedAt' 필드에 대한 설명 주석 추가
    - 'createdAt', 'updatedAt' 필드에 대한 컬럼 설정 변경

--- src/main/kotlin/com/millo/ollim/common/domain/BaseTimeEntity.kt
+++ src/main/kotlin/com/millo/ollim/common/domain/BaseTimeEntity.kt
@@ -1,4 +1,4 @@
-package com.millo.ollim.core.common.entity
+package com.millo.ollim.common.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
@@ -8,14 +8,24 @@ import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
+/**
+ * 생성일시(createdAt), 수정일시(updatedAt)를 자동으로 관리하는 공통 엔티티
+ */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity {
 
+    /**
+     * 생성 시각 (등록 시 자동 저장됨)
+     */
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     lateinit var createdAt: LocalDateTime
 
+    /**
+     * 수정 시각 (변경 시 자동 갱신됨)
+     */
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     lateinit var updatedAt: LocalDateTime
 }
