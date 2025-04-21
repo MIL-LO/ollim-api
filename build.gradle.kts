1962f27c57c03b52ec1c8b69fd2ad7277adbaf3c
악덕 <rudejrqkr97@gmail.com>
2025-04-22 07:09:09 +0900

    #5 refactor: OAuth2 클라이언트 의존성 제거
    - build.gradle.kts 파일에서 OAuth2 클라이언트 의존성을 주석 처리함

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.data:spring-data-rest-hal-explorer")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
