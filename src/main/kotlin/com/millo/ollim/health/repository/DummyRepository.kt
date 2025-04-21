a7ea65ff25514e96fd5dbb6c9550597cd7d9097b
악덕 <rudejrqkr97@gmail.com>
2025-04-21 21:06:52 +0900

    #18 refactor: DummyRepository 및 DummyDocumentRepository 위치 및 패키지 변경
    - DummyRepository는 core.domain.dummy에서 health.repository로 이동
    - DummyDocumentRepository가 새로 생성되었으며, health.repository에 위치

--- src/main/kotlin/com/millo/ollim/health/repository/DummyRepository.kt
+++ src/main/kotlin/com/millo/ollim/health/repository/DummyRepository.kt
@@ -1,5 +1,6 @@
-package com.millo.ollim.core.domain.dummy
+package com.millo.ollim.health.repository
 
+import com.millo.ollim.health.domain.DummyEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
