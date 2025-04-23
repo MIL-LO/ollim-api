467362e2264484d16b515f1230c6fa83c8e67ec3
hyunwoo <daersh@naver.com>
2025-04-24 08:32:51 +0900

    #22 feat(log): 활동 로그 처리 객체 추가
    - AdminActionLogs Entity 생성 및 Config 추가

--- src/main/kotlin/com/millo/ollim/log/config/ActionLogsConfig.kt
+++ src/main/kotlin/com/millo/ollim/log/config/ActionLogsConfig.kt
@@ -5,44 +5,32 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.*
 import org.springframework.stereotype.Component
 
-
+// 모든 요청 처리에 대한 로그를 저장하기 위한 로직입니다.
+// Spring Security 도입 이후 적용 예정
+// 이유: 사용자/관리자 구분이 필요함
 @Aspect
 @Component
-class LogTestConfig {
+class ActionLogsConfig {
 
-    // 메서드 실행 시점을 기준으로 정의
-    // (실행시점(모든타입_클래스_메서드_파라미터 ))
-    @Pointcut(
-        "execution(* com.millo.ollim.health.service..*.*(..)) ||" +
-        "execution(* com.millo.ollim.log.service..*.*(..))" )
+    @Pointcut("execution(* com.millo.ollim.log.*.*(..))")
     fun logMethods() {}
 
-    // Advice - return void
-    // 1. Before
-    // 실행 시점 전
     @Before("logMethods()")
     fun beforeLogMethodExecution(joinPoint: JoinPoint) {
-
         val methodName = joinPoint.signature.name
         val args = joinPoint.args
         println("Before ${methodName}(${args.joinToString(", ")})")
     }
 
-    // 2. AfterReturning - return void
-    // 예외 없이 정상 실행 시점 후
     @AfterReturning("logMethods()")
     fun afterLogMethodExecution(joinPoint: JoinPoint) {
-
         val methodName = joinPoint.signature.name
         val args = joinPoint.args
         println("After ${methodName}(${args.joinToString(", ")})")
     }
 
-    // 3. AfterThrowing - return void
-    // 예외 발생한 경우
     @AfterThrowing("logMethods()")
     fun afterThrowingLogMethodExecution(joinPoint: JoinPoint): Boolean {
-
         val methodName = joinPoint.signature.name
         val args = joinPoint.args
         println("After Throwing ${methodName}(${args.joinToString(", ")})")
@@ -50,17 +38,22 @@ class LogTestConfig {
         return false
     }
 
-    // 4. Around: 메소드 호출 자체를 감싸서, 실행 전후처리 + 메서드 호출 제어 가능
-    // 특정 조건에 따라 메소드 호출 차단 가능
     @Around("logMethods()")
     fun aroundLog(joinPoint: ProceedingJoinPoint): Any? {
-
         val startTime = System.currentTimeMillis()
-        // proceed
-//        if(startTime%2==0L)
         val result = joinPoint.proceed()
         val endTime = System.currentTimeMillis() - startTime
         println("Around endTime = ${endTime}")
         return result
+    }
+
+    fun saveLog(name:String, methodName:String, isAdmin:Boolean, actionType:String ) {
+
+    }
+
+    fun getUserInfo():String{
+        // 사용자 정보 어떻게 가져오는지 확인 필요
+        // 시큐리티 적용하면 SecurityContextHolder
+        return "user"
     }
 }
