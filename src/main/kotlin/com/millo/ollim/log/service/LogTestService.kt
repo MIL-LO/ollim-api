82ba0de8e8a5f170c4ffaef05f9dac384ae5423b
hyunwoo <daersh@naver.com>
2025-04-23 14:12:31 +0900

    #22 refactor(log): LogTestService.kt 중복 제어자 제거
    - 불필요한 중복 제어자 제거했습니다.

--- src/main/kotlin/com/millo/ollim/log/service/LogTestService.kt
+++ src/main/kotlin/com/millo/ollim/log/service/LogTestService.kt
@@ -3,14 +3,14 @@ package com.millo.ollim.log.service
 import org.springframework.stereotype.Service
 
 @Service
-open class LogTestService {
+class LogTestService {
 
-    open fun getSuccess(input:String):String{
+    fun getSuccess(input:String):String{
         println("getSuccess: $input")
         return "Success: ${input}"
     }
 
-    open fun getFailure(input:String):String{
+    fun getFailure(input:String):String{
         println("getFail"+input)
         throw RuntimeException("Failure: ${input}")
     }
