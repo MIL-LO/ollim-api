e47c504563dcdded15fcbae3e3890576e661e1cf
악덕 <rudejrqkr97@gmail.com>
2025-04-23 22:49:24 +0900

    #11 feat: 애플 OAuth 클라이언트 추가
    - SecurityConfig 클래스에 애플 OAuth 클라이언트를 추가함
    - OAuth2 로그인 처리에 애플 OAuth 클라이언트를 사용하도록 설정함

--- src/main/kotlin/com/millo/ollim/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/millo/ollim/common/config/SecurityConfig.kt
@@ -1,18 +1,30 @@
 package com.millo.ollim.common.config
 
+import com.millo.ollim.auth.service.AppleOAuthClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest
 import org.springframework.security.web.SecurityFilterChain
 
 /**
- * Spring Security의 전반적인 보안 설정 클래스입니다.
- * OAuth2 로그인, 인증 필터, 공통 예외처리 등을 포함합니다.
+ * Spring Security의 전반적인 보안 설정 클래스
+ * - OAuth2 로그인, 인증 필터, 공통 예외처리 등을 포함
  */
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val appleOAuthClient: OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
+) {
+
+        @Bean
+    fun appleAccessTokenResponseClient(
+        appleOAuthClient: AppleOAuthClient
+    ): OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> {
+        return appleOAuthClient
+    }
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -26,14 +38,18 @@ class SecurityConfig {
                         "/login/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
-                        "/actuator/**"
+                        "/actuator/**",
+                        "/**"
                     ).permitAll()
                     .anyRequest().authenticated()
             }
             .oauth2Login { oauth2 ->
                 oauth2
-                    // loginPage는 없어도 기본으로 /login 제공됨
                     .defaultSuccessUrl("/auth/oauth-success", true)
+                    .tokenEndpoint { tokenEndpoint ->
+                        tokenEndpoint
+                            .accessTokenResponseClient(appleOAuthClient)
+                    }
             }
             .logout { logout ->
                 logout
