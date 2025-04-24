6cb5c9df2b99681f171f8c8d7d334834059d0a9a
악덕 <rudejrqkr97@gmail.com>
2025-04-24 21:48:52 +0900

    #13 feat: 회원가입 프로필 작성 기능 추가
    - AuthController에 회원가입 시 사용자 프로필을 작성하는 기능 추가
    - UserProfileService를 이용하여 사용자 프로필 생성 및 업데이트 로직 구현
    - '/signup/profile' 엔드포인트를 통해 프로필 정보를 받아 처리

--- src/main/kotlin/com/millo/ollim/auth/controller/AuthController.kt
+++ src/main/kotlin/com/millo/ollim/auth/controller/AuthController.kt
@@ -1,9 +1,15 @@
 package com.millo.ollim.auth.controller
 
 import com.millo.ollim.auth.domain.UserPrincipal
+import com.millo.ollim.user.dto.UserProfileDTO
+import com.millo.ollim.auth.service.AuthService
 import com.millo.ollim.common.enums.Versions
+import com.millo.ollim.user.service.UserProfileService
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("${Versions.V1}/auth")
-class AuthController {
+class AuthController(
+    private val userProfileService: UserProfileService
+) {
 
+    /**
+     * 로그인한 회원 정보 추출 (간단정보)
+     */
     @GetMapping("/me")
     fun getCurrentUser(@AuthenticationPrincipal user: UserPrincipal): Map<String, Any?> {
         return mapOf(
@@ -22,5 +33,17 @@ class AuthController {
             "role" to user.role,
             "nickname" to user.nickname
         )
+    }
+
+    /**
+     * 유저 회원가입 프로필 작성
+     */
+    @PostMapping("/signup/profile")
+    fun submitProfile(
+        @AuthenticationPrincipal user: UserPrincipal,
+        @RequestBody request: UserProfileDTO.Request
+    ) : ResponseEntity<Void> {
+        userProfileService.createOrUpdateProfile(userId = user.userId, request = request)
+        return ResponseEntity.ok().build()
     }
 }
