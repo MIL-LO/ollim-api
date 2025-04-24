fcf08c1e610393233aa6054978662a810355d814
악덕 <rudejrqkr97@gmail.com>
2025-04-24 09:05:52 +0900

    feat: OAuth 인증 기능 추가
    - Application.kt 파일에 OAuth 인증 기능을 추가
    - @EnableJpaRepositories, @ConfigurationPropertiesScan 어노테이션을 추가하여 JPA 리포지토리 활성화 및 구성 속성 스캔을 활성화

--- src/main/kotlin/com/millo/ollim/Application.kt
+++ src/main/kotlin/com/millo/ollim/Application.kt
@@ -1,12 +1,15 @@
 package com.millo.ollim
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories
+@ConfigurationPropertiesScan(basePackages = ["com.millo.ollim"])
 class Application
 
 fun main(args: Array<String>) {
