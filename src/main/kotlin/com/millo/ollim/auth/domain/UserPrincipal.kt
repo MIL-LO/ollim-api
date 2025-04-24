8a51ea9f7466b2c0e56a66281c06f475a591b8be
악덕 <rudejrqkr97@gmail.com>
2025-04-24 19:49:16 +0900

    #20 feat: UserPrincipal 클래스 userId 타입 변경
    - UserPrincipal 클래스의 userId 필드 타입을 String에서 UUID로 변경
    - getName 메소드가 userId를 String으로 반환하도록 수정

--- src/main/kotlin/com/millo/ollim/auth/domain/UserPrincipal.kt
+++ src/main/kotlin/com/millo/ollim/auth/domain/UserPrincipal.kt
@@ -3,19 +3,20 @@ package com.millo.ollim.auth.domain
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.oauth2.core.user.OAuth2User
 import java.io.Serializable
+import java.util.*
 
 /**
  * 인증된 사용자 정보를 담는 Principal
  */
 data class UserPrincipal(
-    val userId: String,
+    val userId: UUID,
     val email: String,
     val role: String,
     val nickname: String? = null,
     private val authorityList: Collection<GrantedAuthority>
 ) : OAuth2User, Serializable {
 
-    override fun getName(): String = userId
+    override fun getName(): String = userId.toString()
 
     override fun getAttributes(): Map<String, Any?> = mapOf(
         "userId" to userId,
