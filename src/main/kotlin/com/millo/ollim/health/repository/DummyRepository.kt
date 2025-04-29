9f9b69fdf24eed1b916929ab4f9f8e81309a13f0
악덕 <rudejrqkr97@gmail.com>
2025-04-29 22:18:36 +0900

    chore: 헬스 체크 관련 코드 제거
    - 헬스 체크 관련 컨트롤러, 도메인, 레포지토리, 서비스 코드 제거
    - 코드 정리를 위한 변경
    - 불필요한 헬스 체크 관련 코드 제거

--- src/main/kotlin/com/millo/ollim/health/repository/DummyRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.millo.ollim.health.repository
-
-import com.millo.ollim.health.domain.DummyEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-@RepositoryRestResource(path = "dummy")
-interface DummyRepository: JpaRepository<DummyEntity, Long> {
-}
