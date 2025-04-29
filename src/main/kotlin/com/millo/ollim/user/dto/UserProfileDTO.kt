a9598263fffc979e3e9562ca47279fefb2926c91
악덕 <rudejrqkr97@gmail.com>
2025-04-29 22:17:50 +0900

    refactor: UserProfileDTO 필드 리팩토링
    - UserProfileDTO의 Request 데이터 클래스에서 필드 정렬을 수정함

--- src/main/kotlin/com/millo/ollim/user/dto/UserProfileDTO.kt
+++ src/main/kotlin/com/millo/ollim/user/dto/UserProfileDTO.kt
@@ -9,15 +9,14 @@ import java.time.LocalDate
 class UserProfileDTO {
 
     data class Request(
-    val nickname: String,
-    val gender: String? = null,
-    val birthDate: LocalDate? = null,
-    val activeTime: String? = null,
-    val energyType: String? = null,
-    val activitySpaces: String? = null,
-    val mbti: String? = null,
-    val profileImage: EmotionCharacterType? = null
-    //TODO: prifileImage는 아직 추가안했지만 추후 추가해서 ENUM으로 관리해야할듯?
+        val nickname: String,
+        val gender: String? = null,
+        val birthDate: LocalDate? = null,
+        val activeTime: String? = null,
+        val energyType: String? = null,
+        val activitySpaces: String? = null,
+        val mbti: String? = null,
+        val profileImage: EmotionCharacterType? = null
     )
 }
 
