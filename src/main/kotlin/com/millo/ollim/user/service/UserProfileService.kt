1cfd72ba9f24f9ba6b0652923a6285ed9ae9eebb
악덕 <rudejrqkr97@gmail.com>
2025-05-02 23:28:29 +0900

    #14 feat: 사용자 프로필 관리 기능 추가
    - UserProfileService에 프로필 생성 및 수정, 프로필 조회, 회원 탈퇴 기능 추가
    - UserProfileServiceImpl에 각 기능의 구현 코드 추가
    - 사용자 상태에 따른 프로필 생성 및 수정 로직 구현
    - 회원 탈퇴 시 사용자 상태 변경 및 탈퇴 시간 기록 추가

--- src/main/kotlin/com/millo/ollim/user/service/UserProfileService.kt
+++ src/main/kotlin/com/millo/ollim/user/service/UserProfileService.kt
@@ -3,6 +3,23 @@ package com.millo.ollim.user.service
 import com.millo.ollim.user.dto.UserProfileDTO
 import java.util.*
 
+/**
+ * 사용자 프로필 관리
+ */
 interface UserProfileService {
+
+    /**
+     * 프로필 생성 또는 수정 (회원가입 시 최초 작성 혹은 이후는 수정)
+     */
     fun createOrUpdateProfile(userId: UUID, userProfileRequest: UserProfileDTO.UserProfileRequest)
+
+    /**
+     * 사용자 프로필 조회 (마이페이지)
+     */
+    fun getProfile(userId: UUID): UserProfileDTO.UserProfileResponse
+
+    /**
+     * 회원 탈퇴
+     */
+    fun withdraw(userId: UUID)
 }
